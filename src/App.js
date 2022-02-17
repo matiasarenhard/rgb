049b1ea989d3779b59b4b9876b3f483e8929f7c5
Matias <matias_arenhard@hotmail.com>
2022-02-16 21:40:41 -0300

    add range btns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+
 
 function App() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <h3>red: {red}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={red}
+        onChange={({ target }) => setRed(parseInt(target.value))} />
+      
+      <h3>green: {green}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={green}
+        onChange={({ target }) => setGreen(parseInt(target.value))} />
+      
+      <h3>blue: {blue}</h3>
+      <input
+        type="range"
+        min={0}
+        max={255}
+        value={blue}
+        onChange={({ target }) => setBlue(parseInt(target.value))} />
+    </>
   );
 }
 
